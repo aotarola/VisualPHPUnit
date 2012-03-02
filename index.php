c498997aba38c3a176150447c896289e66acf239
Nick Sinopoli <NSinopoli@gmail.com>
2012-03-01 23:51:25 -0500

    Cleanup code.

--- index.php
+++ index.php
@@ -1,38 +1,11 @@
 <?php
 
-/* VisualPHPUnit
+/**
+ * VisualPHPUnit
  *
- * Copyright (c) 2011, Nick Sinopoli <nsinopoli@gmail.com>.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in
- *     the documentation and/or other materials provided with the
- *     distribution.
- *
- *   * Neither the name of Nick Sinopoli nor the names of his
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * @author    Nick Sinopoli <NSinopoli@gmail.com>
+ * @copyright Copyright (c) 2011-2012, Nick Sinopoli
+ * @license   http://opensource.org/licenses/bsd-license.php The BSD License
  */
 
     require 'config.php';
@@ -86,7 +59,7 @@
     // Archives
     if ( isset($_POST['view_snapshot']) && $_POST['view_snapshot'] == 1 ) {
         $dir = realpath(SNAPSHOT_DIRECTORY) . '/';
-        $snapshot = realpath($dir . trim(strval(filter_var($_POST['select_snapshot'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES))));
+        $snapshot = realpath($dir . $_POST['select_snapshot']);
 
         ob_start();
         include $snapshot;
@@ -101,10 +74,10 @@
 
     // Graphs
     if ( isset($_POST['graph_type']) ) {
-        $graph_type = trim(strval(filter_var($_POST['graph_type'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES)));
-        $time_frame = trim(strval(filter_var($_POST['time_frame'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES)));
-        $start_date = trim(strval(filter_var($_POST['start_date'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES)));
-        $end_date = trim(strval(filter_var($_POST['end_date'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES)));
+        $graph_type = $_POST['graph_type'];
+        $time_frame = $_POST['time_frame'];
+        $start_date = $_POST['start_date'];
+        $end_date = $_POST['end_date'];
 
         require 'lib/PDO_MySQL.php';
         $config = array(
@@ -121,23 +94,21 @@
     }
 
     // Tests
-
-    // Sanitize all the $_POST data
-    $store_statistics = (boolean) filter_var($_POST['store_statistics'], FILTER_SANITIZE_NUMBER_INT);
-    $create_snapshots = (boolean) filter_var($_POST['create_snapshots'], FILTER_SANITIZE_NUMBER_INT);
-    $snapshot_directory = trim(strval(filter_var($_POST['snapshot_directory'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES)));
-    $sandbox_errors = (boolean) filter_var($_POST['sandbox_errors'], FILTER_SANITIZE_NUMBER_INT);
-    $sandbox_filename = trim(strval(filter_var($_POST['sandbox_filename'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES)));
+    $store_statistics = (boolean) $_POST['store_statistics'];
+    $create_snapshots = (boolean) $_POST['create_snapshots'];
+    $snapshot_directory = $_POST['snapshot_directory'];
+    $sandbox_errors = (boolean) $_POST['sandbox_errors'];
+    $sandbox_filename = $_POST['sandbox_filename'];
     if ( isset($_POST['sandbox_ignore']) && !empty($_POST['sandbox_ignore']) ) {
         $sandbox_ignore = array();
         foreach ( $_POST['sandbox_ignore'] as $ignore ) {
-            $sandbox_ignore[] = trim(strval(filter_var($ignore, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES)));
+            $sandbox_ignore[] = $ignore;
         }
         $sandbox_ignore = implode('|', $sandbox_ignore);
     } else {
         $sandbox_ignore = '';
     }
-    $test_files = trim(strval(filter_var($_POST['test_files'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES)));
+    $test_files = $_POST['test_files'];
     $tests = explode('|', $test_files);
 
     ob_start();
--- lib/PDO_MySQL.php
+++ lib/PDO_MySQL.php
@@ -1,38 +1,11 @@
 <?php
 
-/* VisualPHPUnit
+/**
+ * VisualPHPUnit
  *
- * Copyright (c) 2011, Nick Sinopoli <nsinopoli@gmail.com>.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in
- *     the documentation and/or other materials provided with the
- *     distribution.
- *
- *   * Neither the name of Nick Sinopoli nor the names of his
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * @author    Nick Sinopoli <NSinopoli@gmail.com>
+ * @copyright Copyright (c) 2011-2012, Nick Sinopoli
+ * @license   http://opensource.org/licenses/bsd-license.php The BSD License
  */
 
 class PDO_MySQL {
@@ -44,14 +17,6 @@ class PDO_MySQL {
     *  @access protected
     */
     protected $_dbh;
-
-   /**
-    *  Number of rows affected by MySQL query.
-    *
-    *  @var int
-    *  @access protected
-    */
-    protected $_affected_rows = 0;
 
    /**
     *  The result set associated with a prepared statement.
@@ -110,16 +75,6 @@ class PDO_MySQL {
     }
 
    /**
-    *  Returns the number of rows affected by the last DELETE, INSERT, or UPDATE query.
-    *
-    *  @access public
-    *  @return int
-    */
-    public function affected_rows() {
-    	return $this->_affected_rows;
-    }
-
-   /**
     *  Closes the connection.
     *
     *  @access public
@@ -131,65 +86,16 @@ class PDO_MySQL {
     }
 
    /**
-    *  Deletes a record from the database.
-    *
-    *  @see PDO_MySQL->_format_where()
-    *  @param string $table              The table containing the record to be deleted.
-    *  @param string|array $where        The WHERE clause to be included in the DELETE query.
-    *  @access public
-    *  @return bool
-    */
-    public function delete($table, $where = null) {
-        $sql = 'DELETE FROM `' . $table . '`';
-        $sql .= $this->_format_where($where);
-
-        if ( is_string($where) ) {
-            return $this->query($sql);
-        } elseif ( is_array($where) ) {
-            return $this->query($sql, $where);
-        }
-    }
-
-   /**
-    *  Fetches the next row from the result set in memory (i.e., the one
-    *  that was created after running query()).
-    *
-    *  @param string $fetch_style        Controls how the rows will be returned.
-    *  @access public
-    *  @return mixed
-    */
-    public function fetch($fetch_style = null) {
-        $this->_set_fetch_mode($fetch_style);
-        $row = $this->_statement->fetch();
-        $this->_statement->closeCursor();
-        return $row;
-    }
-
-   /**
     *  Returns an array containing all of the result set rows.
     *
-    *  @param string $fetch_style        Controls how the rows will be returned.
     *  @access public
     *  @return mixed
     */
-    public function fetch_all($fetch_style = null) {
-        $this->_set_fetch_mode($fetch_style);
+    public function fetch_all() {
+        $this->_statement->setFetchMode(\PDO::FETCH_ASSOC);
         $rows = $this->_statement->fetchAll();
         $this->_statement->closeCursor();
         return $rows;
-    }
-
-   /**
-    *  Returns a single column from the next row of a result set or false if there are no more rows.
-    *
-    *  @param int $column_number         Zero-index number of the column to retrieve from the row.
-    *  @access public
-    *  @return mixed
-    */
-    public function fetch_column($column_number = 0) {
-        $column = $this->_statement->fetchColumn($column_number);
-        $this->_statement->closeCursor();
-        return $column;
     }
 
    /**
@@ -324,30 +230,7 @@ class PDO_MySQL {
             die('</pre>');
         }
 
-    	$this->_affected_rows = $statement->rowCount();
         return true;
-    }
-
-   /**
-    *  Returns the ID of the last inserted row or sequence value.
-    *
-    *  @access public
-    *  @return int
-    */
-    public function insert_id() {
-        return $this->_dbh->lastInsertId();
-    }
-
-   /**
-    *  Returns the number of rows affected by the last SELECT query.
-    *
-    *  @access public
-    *  @return int
-    */
-    public function num_rows() {
-        $this->query('SELECT FOUND_ROWS()');
-        $rows = $this->fetch_column();
-        return $rows;
     }
 
    /**
@@ -375,137 +258,10 @@ class PDO_MySQL {
             die('</pre>');
         }
 
-    	$this->_affected_rows = $statement->rowCount();
         $this->_statement = $statement;
     	return true;
     }
 
-   /**
-    *  Executes SQL query and returns the first row of the results.
-    *
-    *  @param string $sql                The SQL query to be executed.
-    *  @param array $parameters          An array containing the parameters to be bound.
-    *  @access public
-    *  @return mixed
-    */
-    public function query_first($sql, $parameters = null) {
-        $this->query($sql . ' LIMIT 1', $parameters);
-    }
-
-   /**
-    *  Sets the fetch mode.
-    *
-    *  @param string $fetch_style        Controls how the rows will be returned.
-    *  @access protected
-    *  @return int
-    */
-    protected function _set_fetch_mode($fetch_style) {
-        switch ( $fetch_style ) {
-            case 'assoc':
-                $this->_statement->setFetchMode(\PDO::FETCH_ASSOC);
-                break;
-            case 'both':
-                $this->_statement->setFetchMode(\PDO::FETCH_BOTH);
-                break;
-            case 'lazy':
-                $this->_statement->setFetchMode(\PDO::FETCH_LAZY);
-                break;
-            case 'num':
-                $this->_statement->setFetchMode(\PDO::FETCH_NUM);
-                break;
-            case 'obj':
-                $this->_statement->setFetchMode(\PDO::FETCH_OBJ);
-                break;
-            default:
-                $this->_statement->setFetchMode(\PDO::FETCH_ASSOC);
-                break;
-        }
-    }
-
-   /**
-    *  Updates a record in the database.
-    *
-    *  @param string $table        The table containing the record to be inserted.
-    *  @param array $data          An array containing the data to be inserted. Format
-    *                              should be as follows:
-    *                              array('column_name' => 'column_value');
-    *  @param array $where         The WHERE clause of the SQL query. Format should
-    *                              be as follows:
-    *                              array('column_name' => 'column_value');
-    *  @access public
-    *  @return bool
-    */
-    public function update($table, $data, $where = null) {
-        $sql = 'UPDATE `' . $table . '` SET ';
-
-        $key_names = array_keys($data);
-    	foreach ( $key_names as $name ) {
-            $sql .= '`' . $name . '`=:' . $name . ', ';
-    	}
-
-        $sql = rtrim($sql, ', ');
-
-        if ( !is_null($where) ) {
-    	    $sql .= ' WHERE ';
-            foreach ( $where as $name => $val ) {
-                $sql .= '`' . $name . '`=:' . $name . '_where, ';
-                $data[$name . '_where'] = $val;
-            }
-        }
-    	$statement = $this->_dbh->prepare($sql);
-
-        try {
-            $statement->execute($data);
-    	} catch ( \PDOException $e ) {
-            echo 'An error occurred: ' . $e->getMessage();
-            echo '<br /><br />Traceback:<pre>';
-            echo var_dump($e->getTrace());
-            die('</pre>');
-        }
-
-    	$this->_affected_rows = $statement->rowCount();
-    	return true;
-    }
-
-   /**
-    *  Inserts or updates (if exists) a record in the database.
-    *
-    *  @param string $table        The table containing the record to be inserted.
-    *  @param array $data          An array containing the data to be inserted. Format
-    *                              should be as follows:
-    *                              array('column_name' => 'column_value');
-    *  @access public
-    *  @return bool
-    */
-    public function upsert($table, $data) {
-        $sql = 'INSERT INTO `' . $table . '` ';
-
-        $key_names = array_keys($data);
-    	$fields = '`' . implode('`, `', $key_names) . '`';
-        $values = ':' . implode(', :', $key_names);
-
-
-        $sql .= '(' . $fields . ') VALUES (' . $values . ') ON DUPLICATE KEY UPDATE ';
-
-    	foreach ( $key_names as $name ) {
-            $sql .= '`' . $name . '`=:' . $name . ', ';
-    	}
-
-        $sql = rtrim($sql, ', ');
-    	$statement = $this->_dbh->prepare($sql);
-
-        try {
-            $statement->execute($data);
-    	} catch ( \PDOException $e ) {
-            echo 'An error occurred: ' . $e->getMessage();
-            echo '<br /><br />Traceback:<pre>';
-            echo var_dump($e->getTrace());
-            die('</pre>');
-        }
-
-    	$this->_affected_rows = $statement->rowCount();
-        return true;
-    }
 }
 
 ?>
